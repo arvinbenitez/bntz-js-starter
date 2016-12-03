b802521460d992e74b80a402fa4cc3dbdbf004a2
arvinbenitez <arvin_benitez@yahoo.com>
2016-12-04 00:13:02 +0800

    Added webpack configuration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev.js'
 
 const port = 3000;
-var app = express();
+const app = express();
+const compiler = webpack(config);
 
-app.get('/', function(req,res){
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPatch: config.output.publicPath
+}));
+
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log(err);
     } else {
         open('http://localhost:' + port);
