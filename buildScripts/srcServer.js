f85d10e1d054d09b50a508a051cb1a9c4c0a2d5c
arvinbenitez <arvin_benitez@yahoo.com>
2016-12-12 23:17:49 +0800

    Added HTTP, Mock HTTP and generating mock data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+    res.json([
+        {"id": 1, "firstName":"Arvin", "lastName": "Benitez", email:"arvin@bntz.com"},
+        {"id": 2, "firstName":"Vince", "lastName": "Benitez", email:"Vince@bntz.com"},
+        {"id": 3, "firstName":"Ann", "lastName": "Benitez", email:"Ann@bntz.com"}
+    ]);
+});
+
 app.listen(port, function (err) {
     if (err) {
         console.log(err);
